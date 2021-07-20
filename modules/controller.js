0e0c61a22cc88c82c66a0a8adff3aaeca93583bf
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-21 01:37:11 +0545

    invalid url added

--- modules/controller.js
+++ modules/controller.js
@@ -3,43 +3,43 @@ var validUrl = require('valid-url');
 
 function Posturl(req, res) {
     const original_url = req.body.url
-    if (validUrl.isUri(original_url)) {
-        urlmodel.findOne({
-            original_url
-        }).exec(function (err, url) {
-            if (err) {
-                return err
-            }
-            if (!url) {
-                var short_url = Math.floor(Math.random() * 100);
-                const newPost = new urlmodel({
-                    original_url: original_url,
-                    short_url: short_url
-                })
-                newPost
-                    .save()
-                    .then(function (data) {
-                        res.status(200).json({
-                            original_url: data.original_url,
-                            short_url: data.short_url
-                        });
-                    })
-                    .catch(function (err) {
-                        console.log("error is>>", err);
-                    });
-            } else {
-                res.json({
-                    original_url: url.original_url,
-                    short_url: url.short_url
-                })
-            }
+    let urlRegex = new RegExp(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi)
+    if (!original_url.match(urlRegex)) {
+        res.json({
+            error: 'Invalid URL'
         })
-
-    } else {
-        res.status(401).json({
-            error: "Invalid URL"
-        })
+        return
     }
+    urlmodel.findOne({
+        original_url
+    }).exec(function (err, url) {
+        if (err) {
+            return err
+        }
+        if (!url) {
+            var short_url = Math.floor(Math.random() * 100);
+            const newPost = new urlmodel({
+                original_url: original_url,
+                short_url: short_url
+            })
+            newPost
+                .save()
+                .then(function (data) {
+                    res.status(200).json({
+                        original_url: data.original_url,
+                        short_url: data.short_url
+                    });
+                })
+                .catch(function (err) {
+                    console.log("error is>>", err);
+                });
+        } else {
+            res.json({
+                original_url: url.original_url,
+                short_url: url.short_url
+            })
+        }
+    })
 }
 
 function getPage(req, res) {
